b46c13c1a8d49c992c108e0f0d218c565bc963dc
justeri10 <ustyuzhaninov01@gmail.com>
2023-01-28 13:54:57 -0300

    first commit

--- hello.py
+++ hello.py
@@ -13,6 +13,9 @@ import numpy as np
 from pathlib import Path
 import gc
 import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import colors
 
 st.set_page_config(
     page_title = "Marketplaces",
@@ -36,13 +39,11 @@ st.text( f"Total len: {len(train)}")
 st.text( f"Total len sellers non empty: {train['Продавец'].count()}")
 st.dataframe(train.head())
 
-
-
-ticket_by_subtopics = (
+with st.expander("Category"):
+    ticket_by_subtopics = (
     train.groupby(by=["Категория"]).count()[["SKU"]].sort_values(by="SKU")
-)
-
-fig_ticket_by_subtopics = px.bar(
+    )
+    fig_ticket_by_subtopics = px.bar(
     ticket_by_subtopics,
     x=ticket_by_subtopics.index,
     y="SKU",
@@ -51,62 +52,19 @@ fig_ticket_by_subtopics = px.bar(
     title="<b>sellers by category</b>",
     color_discrete_sequence=["#0083B8"] * len(ticket_by_subtopics),
     template="plotly_white",
-)
+    )
 
-fig_ticket_by_subtopics.update_layout(
+    fig_ticket_by_subtopics.update_layout(
     plot_bgcolor="rgba(0,0,0,0)",
     xaxis=(dict(showgrid=False))
-)
+    )
 
-st.plotly_chart(fig_ticket_by_subtopics, use_container_width=True)
+    st.plotly_chart(fig_ticket_by_subtopics, use_container_width=True)
 
-ticket_by_topics = (
-    train.groupby(by=["Бренд"]).count()[["SKU"]].sort_values(by="SKU")
-)
-
-#fig_ticket_by_topics = px.bar(
-#    ticket_by_topics,
-#    x=ticket_by_topics.index,
-#    y="SKU",
-#    text="SKU",
-#    orientation="v",
-#    title="<b>sellers by brand</b>",
-#    color_discrete_sequence=["#0083B8"] * len(ticket_by_topics),
-#    template="plotly_white",
-#)
-
-#fig_ticket_by_topics.update_layout(
-#    plot_bgcolor="rgba(0,0,0,0)",
-#    xaxis=(dict(showgrid=False))
-#)
-
-#st.plotly_chart(fig_ticket_by_topics, use_container_width=True)
-
-
-def sla_category(val):
-
-    if val <= 1000000:
-        return '1mi'
-    elif val <= 5000000:
-        return '5mi'
-    elif val <= 10000000:
-        return '10mi'
-    elif val <= 100000000:
-        return '100mi'
-    else:
-        return 'неизвестно'
-
-train['filter'] = train['Выручка30'].apply(sla_category)
-df8 = train.loc[train['filter'].isin(['100mi', '10mi', '5mi'])]
-
-
-
-#st.dataframe(df8)
-df10 = df8[['Продавец',
+df = train[['Продавец',
             'Выручка30',
             'Упущенная выручка',
             'Коммент.',
-            'filter',
             'Дней с продажами',
             'Наличие',
             '2022-12-28',
@@ -142,24 +100,21 @@ df10 = df8[['Продавец',
             'Продаж',
             ]]
 
-df10 = df10.dropna()
-df11 = df10.groupby(['Продавец']).sum()
-st.text( f"Total non empty sellers with income 1 000 000 <= 500 000 000 : {df11['Выручка30'].count()}")
-df11.sort_values(by='Выручка30', ascending=False, inplace=True)
-st.dataframe(df11.style.highlight_max(color = 'lightgreen', axis=0) , 5000, 1000)
+df = df.dropna()
+df = df.groupby(['Продавец']).sum()
+st.text( f"Total len sellers non empty > 5 000 000:")
 
+def sla_category(val):
+    if val >= 5000000:
+        return '5mi'
+    else:
+        return 'неизвестно'
 
-#st.text(f"Heatmap:")
-#df10 = pd.pivot_table(df10, index='date', columns='hour', values='ticket number', aggfunc='count')
-#df10.fillna(0, inplace=True)
-#pivot.sort_values(by='total', ascending=False, inplace=True)
-#piv =  px.imshow(df10)
-#st.plotly_chart(piv, theme=None)
-#df10.sort_values(by='Выручка30', ascending=True, inplace=True)
-#with st.expander("Pivot"):
-#    pivot = pd.pivot_table(df10, index='Продавец', values='filter',aggfunc='count')
-#    pivot.sort_values(by='filter', ascending=True, inplace=True)
-#    pivot = pivot.style.format('{:.0}')\
-#    .format('{:.0f}', subset=['filter'])\
-#    .background_gradient(cmap='ocean_r', subset=['filter'])
-#    st.dataframe(pivot)
+df['filter'] = df['Выручка30'].apply(sla_category)
+
+df1 = df.loc[df['filter'].isin(['5mi'])]
+df1.sort_values(by='Выручка30', ascending=False, inplace=True)
+
+#st.dataframe(df1.style.highlight_max(color = 'lightgreen', axis=0) , 2000, 1000)
+cm = sns.light_palette("#79C", as_cmap=True)
+st.dataframe(df1.style.background_gradient(cm), 2000, 1000)
